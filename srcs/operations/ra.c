14214fbc62ea2eb42b29028e2680417798d29d83
Abderrahman Gaga <agaga@c3r1p3.hive.fi>
2025-03-06 15:35:58 +0200

    modified:   srcs/operations/ra.c 	modified:   srcs/operations/rr.c 	modified:   testlinux.sh

--- srcs/operations/ra.c
+++ srcs/operations/ra.c
@@ -6,14 +6,14 @@
 /*   By: agaga <agaga@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 19:03:54 by agaga             #+#    #+#             */
-/*   Updated: 2025/02/06 18:56:09 by agaga            ###   ########.fr       */
+/*   Updated: 2025/02/10 14:19:23 by agaga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
 // ra (rotate a) : shift up all elements of stack a by 1. 
-// The first element becomes the last one.
+//  The first element becomes the last one.
 void	ft_ra(t_stack **a)
 {
 	t_stack	*tmp;
